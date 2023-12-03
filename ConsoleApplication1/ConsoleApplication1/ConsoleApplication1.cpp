7d6c97a00642682a3aa0a06872b9b60181df57e0
orlovNikitq <130779328+orlovNikitq@users.noreply.github.com>
2023-12-03 13:35:19 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <iostream>
+#include <unordered_map>
+
+class ImageDownloader {
+public:
+    virtual void downloadImage(const std::string& url) = 0;
+};
+
+class RealImageDownloader : public ImageDownloader {
+public:
+    void downloadImage(const std::string& url) override {
+        std::cout << "Загрузка изображения с " << url << std::endl;
+    }
+};
+
+class ImageProxy : public ImageDownloader {
+private:
+    RealImageDownloader realDownloader;
+    std::unordered_map<std::string, bool> imageCache;
+
+public:
+    void downloadImage(const std::string& url) override {
+        if (imageCache.find(url) != imageCache.end()) {
+            std::cout << "Изображение уже находится в кэше." << std::endl;
+        }
+        else {
+            std::cout << "Изображения нет в кэше. Загрузка." << std::endl;
+            realDownloader.downloadImage(url);
+            imageCache[url] = true;
+        }
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    ImageProxy imageProxy;
+    imageProxy.downloadImage("/image1.jpg");
+    imageProxy.downloadImage("/image2.jpg");
+    imageProxy.downloadImage("/image1.jpg");  
+
+    return 0;
+}
+
